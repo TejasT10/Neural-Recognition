6f0df672c4793980bb9386e270f6ff7325a4e5e5
Tejas Shende <tejasgshende@gmail.com>
2023-10-12 02:18:36 +0530

    Create Basecode.py
    Basic code for neural network creation

---
+++ Basecode.py
@@ -0,0 +1,31 @@
+# Import necessary libraries
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+# Load the MNIST dataset
+mnist = keras.datasets.mnist
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+# Preprocess the data
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+# Build the neural network model
+model = keras.Sequential([
+    layers.Flatten(input_shape=(28, 28)),
+    layers.Dense(128, activation='relu'),
+    layers.Dropout(0.2),
+    layers.Dense(10)
+])
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+# Train the model
+model.fit(train_images, train_labels, epochs=5)
+
+# Evaluate the model
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+print(f"Test accuracy: {test_acc}")
