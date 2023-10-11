6ead35ec9ca61fea90d0ab4d009a2ab2a2571e67
Tejas Shende <tejasgshende@gmail.com>
2023-10-12 02:31:25 +0530

    Create dataprep.py

---
+++ dataprep.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+from tensorflow import keras
+
+mnist = keras.datasets.mnist
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+train_images, test_images = train_images / 255.0, test_images / 255.0
